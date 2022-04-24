1e01f74ad677a7599cdd8001532d3c8de1609a5e
m1cks <sjin3568@gmail.com>
2022-04-24 10:45:08 +0900

    add search(url, view)

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -18,3 +20,31 @@ class RoomDetail(DetailView):
     """RoomDetail Definition"""
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", "0"))
+    room_types = models.RoomType.objects.all()
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
+    return render(
+        request,
+        "rooms/search.html",
+        {
+            **form,
+            **choices,
+        },
+    )
