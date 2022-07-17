83addf401c4a047293180f9b55644e77de70c44c
m1cks <sjin3568@gmail.com>
2022-07-18 08:20:47 +0900

    add placeholder to password field

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,9 @@ from . import models
 
 class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         email = self.cleaned_data.get("email")
