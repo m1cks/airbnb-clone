813f8079ab6cbe47636bb47335b73ec42004b872
m1cks <sjin3568@gmail.com>
2022-07-18 08:36:08 +0900

    add bookedday, modify status_confirmed, add custom object manager, modify is_finished method, override  save method in reservation model for check booked day and create booked day

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,21 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+from . import managers
+
+
+class BookedDay(core_models.TimeStampedModel):
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+    def __str__(self):
+        return str(self.day)
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -8,7 +23,7 @@ class Reservation(core_models.TimeStampedModel):
     """Reservation Model Defenition"""
 
     STATUS_PENDING = "pending"
-    STATUS_CONFIRMED = "confirm"
+    STATUS_CONFIRMED = "confirmed"
     STATUS_CANCELED = "canceled"
 
     STATUS_CHOICES = (
@@ -29,6 +44,8 @@ class Reservation(core_models.TimeStampedModel):
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
 
+    objects = managers.CustomReservationManager()
+
     def __str__(self):
         return f"{self.room} - {self.check_in}"
 
@@ -40,6 +57,26 @@ class Reservation(core_models.TimeStampedModel):
 
     def is_finished(self):
         now = timezone.now().date()
-        return now > self.check_out
+        is_finished = now > self.check_out
+        if is_finished:
+            BookedDay.objects.filter(reservation=self).delete()
+        return is_finished
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            existing_booked_day = BookedDay.objects.filter(
+                reservation__room=self.room, day__range=(start, end)
+            ).exists()
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
+
+        return super().save(*args, **kwargs)
